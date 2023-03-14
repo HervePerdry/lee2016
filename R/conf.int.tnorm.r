649e4ca910ef7a89fcff381cbb0542e3783e6eee
HervePerdry <herve.perdry@gmail.com>
2023-03-14 14:19:19 +0100

    improved conf.int.tnorm

--- R/conf.int.tnorm.r
+++ R/conf.int.tnorm.r
@@ -6,15 +6,31 @@
 #' @param low
 #' @param upp
 #'
-#' @details this function needs improvement. 
+#' @details this function finds a confidence interval by solving the equation
+#' \code{ptnorm(x0, mu, sd, low, upp) = alpha/2} or \code{1 - alpha/2}, \code{mu}
+#' being the unknown. 
+#' @details It corresponds to Theorem 6.1.
 #' @return a vector of length 2
 #' @examples conf.int.tnorm(0)
 #' conf.int.tnorm(0, low = -1, upp = +Inf)
 #' @export
 conf.int.tnorm <- function(x0, level = 0.95, alpha = 1 - level, sd = 1, low = -Inf, upp = +Inf) {
-  # on cherche mu tel que ptnorm(x0, mu, ...) = 0.025, 0.975
-  f <- \(mu, alpha) ptnorm(x0, mu, sd, low, upp) - alpha
-  U <- uniroot(f, x0 + c(-5, 5)*sd, alpha = alpha/2)$root
-  L <- uniroot(f, x0 + c(-5, 5)*sd, alpha = 1 - alpha/2)$root
+  f <- function(mu, alpha) ptnorm(x0, mu, sd, low, upp) - alpha
+  sol <- function(a) { 
+    k <- 4;
+    repeat {
+      if(is.infinite(k))
+        stop("Failed") 
+      R <- try( uniroot(f, x0 + c(-1, 1)*k, alpha = a)$root, TRUE )
+      if(class(R) != "numeric") 
+        k <- k*2
+      else
+        break
+    }
+    R
+  }
+  U <- sol(alpha/2)
+  L <- sol(1 - alpha/2)
   c(L, U)
 }
+
